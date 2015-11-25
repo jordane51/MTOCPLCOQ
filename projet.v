ac507c9e7c96dbe526a58b0694dfa30c310953d0
Remi Schiano <rschiano@emi.u-bordeaux1.fr>
2015-11-25 09:32:42 +0100

    Toutes les fonctions génériques ok

--- projet.v
+++ projet.v
@@ -23,7 +23,8 @@ Require Import ZArith.
 Section poly.
 
 Variables (T:Type)
-          (leb:T->T->bool).
+          (leb:T->T->bool)
+          (eqb:T->T->bool).
 
 Fixpoint insert (z:T) (l:list T) {struct l} : list T :=
   match l with
@@ -39,10 +40,42 @@ match l with
 | z::l' => insert z (insertion_sort l')
 end.
 
-End poly.
+Fixpoint nb_occ (z:T) (l:list T) {struct l} : nat :=
+  match l with
+  | nil => 0%nat
+  | (z' :: l') =>
+      if eqb z z' 
+      then S (nb_occ z l')
+      else nb_occ z l'
+  end.
 
+Definition equiv (l l':list T) := 
+    forall z:T, nb_occ z l = nb_occ z l'.
+
+End poly.
 
 About insert.
 Open Scope Z_scope.
+
+Section Tests.
 Compute insert Z Z.leb 4 (2 :: 5 :: nil).
 Compute insertion_sort Z Z.leb (4::9::7::9::12::3::nil).
+Eval compute in (nb_occ Z Z.eqb 3 (3 :: 7 :: 3 :: nil)).
+End Tests.
+
+Section Proofs.
+Inductive sorted : list Z -> Prop :=
+  | sorted0 : sorted nil
+  | sorted1 : forall z:Z, sorted (z :: nil)
+  | sorted2 :
+      forall (z1 z2:Z) (l:list Z),
+        z1 <= z2 ->
+        sorted (z2 :: l) -> sorted (z1 :: z2 :: l).
+
+Hint Resolve sorted0 sorted1 sorted2 : sort.
+Lemma sort_2357 :
+ sorted (2 :: 3 :: 5 :: 7 :: nil).
+Proof.
+ auto with sort zarith.
+Qed.
+End Proofs.
